51c1883563409ed77ad830540a18f2b73b85348c
LuisGuilhermeCipriani <cipriani.luisguilherme@gmail.com>
2020-04-15 13:58:21 -0300

    Criação da classe Cliente

---
+++ PetShop_JS/src/model/Cliente.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author cipri
+ */
+public class Cliente {
+    private String nome;
+    private String CPF;
+    private int idade;
+    private int id;
+
+    public Cliente() {
+    }
+
+    public Cliente(String nome, String CPF, int idade, int id) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.idade = idade;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+}
--- PetShop_JS/src/model/Gato.java
+++ PetShop_JS/src/model/Gato.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package petshop_js;
+package model;
 
 /**
  *
  * @author cipri
  */
-public class PetShop_JS {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Gato {
     
 }
